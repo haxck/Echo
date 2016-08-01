88362904aed750e40464346202a270d2b263eac8
zhiqiang Du <haxck.x@gmail.com>
2016-08-01 21:01:17 +0800

    init express

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+var db = require('./db.js');
+var hbs = require('hbs');
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'hbs');  // 用hbs作为模版引擎
+app.set('views', __dirname); // 模版所在路径
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public'));//配置静态资源文件
+
+
+
+//添加一位用户
+app.get('/', function(req, res) {
+	res.render('index');
+})
+app.get('/article/:shareStr', function(req, res) {	
+	db.selectArticle({shareStr:req.params.shareStr},function(article){
+		console.log('select: ' + JSON.stringify(article));
+		console.log(article.title + '---' + article.content);
+		res.render('view',article);		
+	});
+})
+app.post('/new_article', function(req, res) {
+	var str = "";
+
+	str = generateStr();
+	console.log('生成随机字符串');
+	db.newArticle({shareStr:str,title:req.body.title,content:req.body.content});				
+	res.send(str);
+	//res.render('view',{'title':'sd','content':'sdsss'});
+})
+
+
+//处理404
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+//处理500
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+//创建server
+var server = app.listen(80, function() {
+	var host = server.address().address
+	var port = server.address().port
+	console.log(" app listening at http://%s:%s", host, port)
+
+});
+
+
+function generateStr(){
+	var dataArr = new Array();
+	for(var i=0;i<26;i++){
+		dataArr.push(String.fromCharCode('A'.charCodeAt() + i));
+	}
+	for(var i=0;i<10;i++){
+		dataArr.push(i+'');
+	}
+	var str = "";
+	var n = "";
+	for(var i = 0;i<8;i++){
+		n =dataArr[parseInt(Math.random()*35-1)];
+		str += n;
+	}
+	return str;
+}
---
+++ db.js
@@ -0,0 +1,113 @@
+var mongoClient = require('mongodb').MongoClient;
+var conn_str = 'mongodb://localhost:27017/island';
+var collectionName = 'article';
+var db;
+// var Schema = mongoose.Schema;   //  创建模型
+// var articleScheMa = new Schema({
+// 	title: String,
+// 	content: String,
+// 	shareStr: String
+// }); //  定义了一个新的模型，但是此模式还未和article集合有关联
+// exports.article = db.model('article', articleScheMa); //  与article集合关联
+
+
+//数据库 island
+
+exports.newArticle = function(data,callback) {
+	console.log('添加数据' + data);
+	connect_db(function(err,db){
+		this.db = db;
+		var collection = db.collection(collectionName);
+		collection.insert(data,function(err,result){
+			if (err) {
+				console.log('Error:' + err);
+				return;
+			}
+			db.close();
+			if (callback != null) {
+				callback();
+			}
+		});
+	});
+	
+};
+
+exports.selectArticle = function(whereStr,callback) {  
+
+	connect_db(function(err,db){
+		//连接到表  
+		var collection = db.collection(collectionName);
+		collection.find(whereStr).toArray(function(err, result) {
+			if(err){
+				console.log('Error:'+ err);
+				return;
+			}
+			console.log('查询数据' + result[0]);   
+			callback(result[0]);
+			db.close();
+		});		
+	});
+	
+}
+//指定分享url是否存在
+exports.exist = function(whereStr,callback){
+	connect_db(function(err,db){
+		//连接到表  
+		var collection = db.collection(collectionName);
+		collection.find(whereStr).toArray(function(err, result) {
+			if(err){
+				console.log('Error:'+ err);
+				return;
+			}     
+			db.close();
+			if (result.length > 0) {
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});		
+	});
+}
+//链接数据库
+function connect_db(callback){
+	mongoClient.connect(conn_str,function(err,db){
+		callback(err,db);
+	});
+}
+
+
+
+
+
+
+/**
+
+//更新数据
+  var whereStr = {"name":'wilson001'};
+  var updateStr = {$set: { "age" : 100 }};
+  collection.update(whereStr,updateStr, function(err, result) {
+    if(err)
+    {
+      console.log('Error:'+ err);
+      return;
+    }	 
+    callback(result);
+  });
+
+  var delData = function(db, callback) {  
+  //连接到表  
+  var collection = db.collection('tb2');
+  //删除数据
+  var whereStr = {"name":'wilson001'};
+  collection.remove(whereStr, function(err, result) {
+    if(err)
+    {
+      console.log('Error:'+ err);
+      return;
+    }     
+    callback(result);
+  });
+}
+
+
+*/
