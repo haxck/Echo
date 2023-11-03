9b0cf10c599a8bd7aa9332e1945b274e05cda109
Zhiqiang Du <haxck@qq.com>
2023-11-03 16:19:26 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Flex, Text, Button,TextField, Box, Container } from '@radix-ui/themes';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [code,setCode] = useState(null)
 
+  const onAction = () => {
+    alert(code);
+  }
   return (
     <>
+      <Container size={3}>
+
+
       <h1>Echo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Flex direction="column" gap="3" width={9} >
+        <TextField.Root>
+          <TextField.Input placeholder="请输入取信码" value={code} onChange={(e)=>{setCode(e.target.value)}} style={{textAlign: "center"}}/>
+        </TextField.Root>
+        <Button onClick={onAction}>Let's go</Button>
+      </Flex>
+      </Container>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from './routers/root'
+import Letter from './routers/letter'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "letter/:letterId",
+    loader: async ({ params }) => {
+      const data = await fetch(`http://192.168.103.36:3000/echo/${params.letterId}`)
+        .then(res => res.json())
+
+      return data
+    },
+    element: <Letter />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Theme appearance="light" accentColor="red" radius="full" scaling="110%">
+      <RouterProvider router={router} ></RouterProvider>
+    </Theme>
   </React.StrictMode>,
 )
